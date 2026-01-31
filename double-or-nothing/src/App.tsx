cee4973f20e06da8e015e7ac9633468ea2d843b4
Kevin <alegre.kev@gmail.com>
2026-01-31 14:05:08 -0500

    Initial commit

---
+++ double-or-nothing/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black font-mono text-blue-100 text-2xl font-bold">
+      Double or Nothing 🎰
+    </div>
+  )
+}
+
+export default App
