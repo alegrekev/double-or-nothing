341df451eb28c411fb4af84b9182c25939151320
Kevin <alegre.kev@gmail.com>
2026-01-31 14:46:05 -0500

    Big changes

---
+++ double-or-nothing/src/components/Question.tsx
@@ -0,0 +1,21 @@
+export default function GamePage() {
+    return (
+        <div className="font-mono">
+            <div className="m-auto mt-25 w-100 flex items-center justify-center text-2xl font-bold">
+                Question
+            </div>
+            <div className="m-auto mt-10 w-230 border-red-400 flex items-center justify-center">
+                <div className="m-auto w-100 p-2 border-2 rounded-2xl text-center items-center justify-center text-xl">
+                    Answer 1<br></br>
+                    Uh, I can't come to your party
+                    I might come just to hurt you (schyeah, schyeah)
+                </div>
+                <div className="m-auto w-100 p-2 border-2 rounded-2xl text-center items-center justify-center text-xl">
+                    Answer 2<br></br>
+                    Uh, I can't come to your party
+                    I might come just to hurt you (schyeah, schyeah)
+                </div>
+            </div>
+        </div>
+    )
+}
